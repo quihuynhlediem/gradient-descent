8c26d4ad8ecdec438941233361f2cc913d420688
Qui Huynh Le Diem <huynhlediemqui.vn@gmail.com>
2023-09-08 10:40:21 +0700

    Update gradient_descent

--- gradient_descent.py
+++ gradient_descent.py
@@ -7,19 +7,19 @@ import math
 
 
 def gradient_descent_process(starting_point_v, starting_point_b, learning_rate, precision, max_iterations):
-    clear_graph_folder()
+    #clear_graph_folder()
 
     v_old = starting_point_v
     b_old = starting_point_b
 
     i = 0
 
-    f = LinearRegression(v_old, b_old)
+    f = LinearRegression()
 
-    v = np.linspace(30, 40, 100)
-    b = np.linspace(10, 20, 100)
+    v = np.linspace(-100, 100, 1000)
+    b = np.linspace(-100, 100, 1000)
 
-    # plot_function_contour(v, b)
+    #plot_function_contour(f, v, b)
 
     while True:
         if (v_old < v[0]):  # limit the value of gradient
@@ -35,8 +35,8 @@ def gradient_descent_process(starting_point_v, starting_point_b, learning_rate, 
         print("v_old:", v_old)
         print("b_old:", b_old)
 
-        v_gradient = f.gradient()[0]
-        b_gradient = f.gradient()[1]
+        v_gradient = f.gradient(v_old, b_old)[0]
+        b_gradient = f.gradient(v_old, b_old)[1]
 
         print("x_gradient:", v_gradient)
         print("y_gradient:", b_gradient)
@@ -46,11 +46,11 @@ def gradient_descent_process(starting_point_v, starting_point_b, learning_rate, 
 
         print("v_new:", v_new)
         print("b_new:", b_new)
-        f = LinearRegression(v_new, b_new)
-        print("z: ", f.value())
+        print("z: ", f.value(v_new, b_new))
+        z_value = f.value(v_new, b_new)
         print('---------------')
 
-        # plot_changes(f, v, v_old, v_new, b, b_old, b_new, '' + str(i))
+        #plot_changes(f, v, v_old, v_new, b, b_old, b_new, '' + str(i))
         if math.sqrt((v_new - v_old)**2 + (b_new - b_old)**2) < precision:
             print("Precision reached!")
             break
@@ -61,8 +61,22 @@ def gradient_descent_process(starting_point_v, starting_point_b, learning_rate, 
         v_old = v_new
         b_old = b_new
         i += 1
+    return(z_value)
+    #create_gifs()
 
-    # create_gifs()
+
+def fluctuate_number(min):
+    i = 0
+    for a in range(3):
+        for b in range(3):
+            z_value = gradient_descent_process(a, b, 0.001, 0.00001, 200)
+            if (z_value < min):
+                min = z_value
+            i += 1
+    return([min, i])
 
 
-gradient_descent_process(35, 15, 0.005, 0.0000001, 200)
+min = 100000000
+
+
+print(fluctuate_number(min))

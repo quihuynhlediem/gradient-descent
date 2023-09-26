7aec45e8c416e68d372f189121ac0866c688da6e
Qui Huynh Le Diem <huynhlediemqui.vn@gmail.com>
2023-09-26 10:26:15 +0700

    Basic update for log loss

--- math_function.py
+++ math_function.py
@@ -5,42 +5,14 @@ exam_result = np.array([0, 0, 0, 0, 0, 0, 1, 0, 1, 0,
                        1, 0, 1, 0, 1, 1, 1, 1, 1, 1])
 
 
-class LossFunction():
+class LogLoss():
     def __init__(self):
-        self.threshold = 1
-        self.additional_variable = 1
-
-    def sigmoid(self, x):
-        return 1 / (1 + np.exp(-x))
-
-    def predict(self, study_time):
-        return self.sigmoid(self.additional_variable * study_time - self.threshold)
-
-    def value(self, threshold, additional_variable):
-        self.additional_variable = additional_variable
+        self.threshold = 0
+        self.coefficient = 0
+    
+    def value(self, threshold, coefficient):
         self.threshold = threshold
-        self.function = 0
-        for i in range(len(total_study_time)):
+        self.coefficient = coefficient
+        for i in range(len(exam_result)):
             result = exam_result[i]
-            study_time = total_study_time[i]
-            self.function += (result - self.predict(study_time)) ** 2
-        return self.function
-
-    def deriv(self, threshold, additional_variable):
-        self.additional_variable = additional_variable
-        self.threshold = threshold
-        self.derivative_threshold = 0
-        self.derivative_additional_variable = 0
-        for i in range(len(total_study_time)):
-            result = exam_result[i]
-            study_time = total_study_time[i]
-            self.derivative_threshold += 2 * \
-                (result - self.predict(study_time)) * \
-                self.predict(study_time) * \
-                (1 - self.predict(study_time))
-
-            self.derivative_additional_variable += -2 * \
-                (result - self.predict(study_time)) * \
-                self.predict(study_time) * \
-                (1 - self.predict(study_time)) * study_time
-        return np.array([self.derivative_additional_variable, self.derivative_threshold])
+            
